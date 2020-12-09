d95f38c2a228165db9837f74bc2b06b8a5b294e7
prangamajumder <pranganmajumder@appscode.com>
2020-12-09 19:12:53 +0600

    c31: added first interfaces area() for c1 an r1

--- chapter9/interfaces.go
+++ chapter9/interfaces.go
@@ -14,11 +14,16 @@ type rect struct {
 	weidth float32
 }
 
-func (c *circle) area() float32 {
+type shape interface {
+	area() float32
+	//pow() float32
+}
+
+func (c circle) area() float32 { // dont use pointer c * circle
 	return math.Pi * c.radius * c.radius
 }
 
-func (r *rect) area() float32  {
+func (r rect) area() float32  { // dont use pointer
 	return r.height * r.weidth
 }
 
@@ -26,6 +31,16 @@ func main()  {
 	c1 := circle{4.5}
 	r1 := rect{5,7}
 
-	fmt.Println("circle area  " , c1.area())
-	fmt.Println("rectangle area " , r1.area())
+	//fmt.Println("circle area  " , c1.area())
+	//fmt.Println("rectangle area " , r1.area())
+
+	shapes := []shape {c1 , r1}
+
+	for _, shape := range shapes{
+		fmt.Println(shape.area())
+	}
+
+
+
+
 }
