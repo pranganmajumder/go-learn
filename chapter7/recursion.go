89666f11cf6515d12ff200e5de1feafe71ee9f0e
prangamajumder <pranganmajumder@appscode.com>
2020-12-09 10:01:57 +0600

    c:13  created recursion.go and added getsum()

---
+++ chapter7/recursion.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func getSum(pos int , ar [6]int) int {
+	if pos == len(ar) {
+		return 0
+	}
+	return  ar[pos] + getSum(pos+1, ar)
+}
+
+func main()  {
+	ar := [...]int {2,4,6,7,3,6}
+	sum := getSum(0, ar)
+	fmt.Println("sum is   =  ", sum)
+}
