53034dd43731188a4898ca994a1092e6f10d1cdb
prangamajumder <pranganmajumder@appscode.com>
2020-12-10 17:58:03 +0600

    c55 shorter way to read file

---
+++ chapter13/filesAndFolders2.go
@@ -0,0 +1,19 @@
+// shorter way to read a file
+
+package  main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main()  {
+	bs, err := ioutil.ReadFile("test.txt")
+	if err != nil{
+		// handle error
+		return
+	}
+	str := string(bs)
+
+	fmt.Println(str)
+}
