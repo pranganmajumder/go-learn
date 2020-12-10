8c38c240127d6a10551c54457b3235653a173741
prangamajumder <pranganmajumder@appscode.com>
2020-12-10 18:16:23 +0600

    c57: updated file

---
+++ chapter13/createAndWriteIntoFile.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	file, err := os.Create("test2.txt")
+	if err != nil{
+		// handle the error there
+		return
+	}
+	defer file.Close()
+	file.WriteString("this is our newly created file test2.txt")
+
+
+
+}
