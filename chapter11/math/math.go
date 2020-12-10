882501953939eb1bd9625c108fd15131fe1a67f2
prangamajumder <pranganmajumder@appscode.com>
2020-12-10 12:03:44 +0600

    c44:added Max() Min() to our packages

--- chapter11/math/math.go
+++ chapter11/math/math.go
@@ -7,3 +7,23 @@ func Average(xs ... float32)float32  {
 	}
 	return total / float32(len(xs))
 }
+
+func Max(xs ... float32)float32  {
+	mx := float32(0)
+	for _, v := range xs{
+		if v > mx {
+			mx = v
+		}
+	}
+	return mx
+}
+
+func Min(xs ... float32)float32  {
+	mn := float32(9999999)
+	for _, v := range xs{
+		if v < mn{
+			mn = v
+		}
+	}
+	return  mn
+}
