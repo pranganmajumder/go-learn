df5ff09d3be0c0fb8d9d24d86437717ffd3e8451
prangamajumder <pranganmajumder@appscode.com>
2020-12-10 12:03:10 +0600

    c43:added math.Max(xs...) and math.Min(xs...)

--- chapter11/main.go
+++ chapter11/main.go
@@ -9,4 +9,7 @@ func main()  {
 	xs := []float32{1,4,5,2, 1}
 	avg := math.Average(xs...)
 	fmt.Println(avg)
+
+	fmt.Println("max of xs  :  ", math.Max(xs...))
+	fmt.Println("min of xs   : ", math.Min(xs...))
 }
