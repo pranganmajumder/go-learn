f42c4986cdfe337f3bf264db1897241de1394e96
prangamajumder <pranganmajumder@appscode.com>
2020-12-14 13:05:20 +0600

    c67: updated command_line_flag.go

--- myFlags/command_line_flags.go
+++ myFlags/command_line_flags.go
@@ -11,15 +11,15 @@ func main() {
 	numbPtr := flag.Int("numb" , 42 , "an integer")
 	boolPtr := flag.Bool("fork" , false , "a bool")
 
-	var svar string
-	flag.StringVar(&svar , "svar" , "bar" , "a string var")
+	var svarr string
+	flag.StringVar(&svarr , "svar" , "bar" , "a string var")
 
 	flag.Parse()
 
 	fmt.Println("word : " , *wordPtr)
 	fmt.Println("numb : " , *numbPtr)
 	fmt.Println("fork : " , *boolPtr)
-	fmt.Println("tail : " , svar)
+	fmt.Println("tail : " , svarr)
 	fmt.Println("tail: " , flag.Args())
 
 }
